63d24d70d063b77f4e206035d2c1884e0215e4ce
Odailton Marinho <odailton_marinho1000@hotmail.com>
2014-09-23 20:34:32 -0300

    Instanciação sem erros, porem só instancia cubos

---
+++ Tutorial/Odailton/Inst_sem_erros/Assets/G.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class G : MonoBehaviour {
+
+	public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+		GameObject a = (GameObject) Instantiate(g);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (0, 40, 400, 40), "Quartel Jandui")) 
+		{
+
+		}
+		
+	}
+}
