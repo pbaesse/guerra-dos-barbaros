6c6ac01062795209e2e26e83085ec18c5af8a227
thyagofernandes <thyagomau2@gmail.com>
2014-10-27 01:47:06 -0300

    desvio e maquina de estado 1.0

---
+++ Tutorial/Thyago_codes/desvio/Assets/Scripts/unidade_inimiga.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unidade_inimiga : MonoBehaviour {
+	public int vida = 60;
+	public int forca = 10;
+	public Vector3 alvo;
+	private NavMeshAgent agente;
+	static string a ;
+	// Use this for initialization
+	void Start () 
+	{
+		agente = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		a = Controlador.get_nome();
+	}
+	void ataque()
+	{
+
+	}
+}
