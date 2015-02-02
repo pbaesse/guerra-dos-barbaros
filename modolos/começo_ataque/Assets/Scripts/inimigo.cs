243f111a6dfa69ec34277bb3d123844886b8fcde
thyagofernandes <thyagomau2@gmail.com>
2015-02-02 20:26:31 -0300

    começo de ataque com o inimigo vendo e atacando a unidade aliada

---
+++ modolos/começo_ataque/Assets/Scripts/inimigo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class inimigo : MonoBehaviour {
+	public static List<GameObject> unidades_inimigas;
+	public int forca;
+	public int vida;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		unidades_inimigas = new List<GameObject>(GameObject.FindGameObjectsWithTag("unidade"));
+		foreach (GameObject unidade in unidades_inimigas)
+		{
+			float distancia = Vector3.Distance(unidade.transform.position, this.transform.position);
+			Debug.Log (distancia+ unidade.name);
+			if (distancia <= 15){
+				Debug.Log ("PERTO"+ unidade.name);
+				unidade.SendMessage("atacar_unidade", forca, SendMessageOptions.DontRequireReceiver);
+			}
+			else
+						Debug.Log ("LONGE"+ unidade.name);	
+		}
+
+
+	}
+	void atacar_inimigo(int vida){}
+
+}
---
+++ modolos/começo_ataque/Assets/Scripts/unidades.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+//
+public class unidades : MonoBehaviour {
+	// para o mouse
+
+	public  bool selecionado = false;
+	private bool salva_f1 = false;
+	private bool salva_f2 = false;
+	public int vida;
+
+
+	void Start()
+	{
+		StartCoroutine(esperar ());
+	}
+	void LateUpdate () 
+	{
+		verifica_morte ();
+	}
+	public  void OnSelected()
+	{
+		Debug.Log ("sele");
+		if (!Input.GetKey (KeyCode.LeftControl)) 
+		{
+						selecionado = true;
+						renderer.material.color = Color.red;
+		}
+		if(Input.GetKey (KeyCode.LeftControl) && selecionado)
+		{
+			Debug.Log ("dessele");
+			selecionado = false;
+			renderer.material.color = Color.white;
+		}
+		//Mouse.unidades_selecionandas.Add(this.transform.gameObject);
+		else if(Input.GetKey (KeyCode.LeftControl) && !selecionado)
+		{
+			Debug.Log ("dessele");
+			selecionado = true;
+			renderer.material.color = Color.red;
+		}
+
+	}
+	
+	public void OnUnselected()
+	{
+		selecionado = false;
+		renderer.material.color = Color.white;
+	
+		//Mouse.unidades_selecionandas.Remove(this.transform.gameObject);
+	}
+	public void salvar(KeyCode a)
+	{
+		if(selecionado && a == KeyCode.F1)
+		salva_f1 = true;
+		if(selecionado && a == KeyCode.F2)
+			salva_f2 = true;
+
+	
+	}
+	public void  carregar(KeyCode a)
+	{
+		if(salva_f1 && a == KeyCode.F6)
+		{
+			selecionado = true;
+			renderer.material.color = Color.red;
+		}
+		if(salva_f2 && a == KeyCode.F7)
+		{
+			selecionado = true;
+			renderer.material.color = Color.red;
+		}
+	}
+	public void atacar_unidade(int forca)
+	{
+		Debug.Log ("atacado");
+		vida -= forca;
+		esperar ();
+	}
+	void verifica_morte ()
+	{
+		if(vida == 0)
+		Destroy (this.gameObject);
+	}
+	IEnumerator esperar()
+	{
+		yield return new WaitForSeconds (10);
+	}
+}
+
